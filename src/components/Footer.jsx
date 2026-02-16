d0468b33cf69b674784c042e646903966f000b70
alessiodalberto002 <aledalberto002.com>
2026-02-16 20:55:09 +0100

    cambiamento del footer attraverso i dati scritti tramite oggetto letterale

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,40 +3,49 @@ const footerSection = [
         id: 1,
         title: "dc comics",
         links: [
-            {
-                label: "Characteers",
-                href: "#",
-            }
+            { label: "Characters", href: "#"},
+            { label: "Comics", href: "#"},
+            { label: "Movies", href: "#"},
+            { label: "TV", href: "#"},
+            { label: "Games", href: "#"},
+            { label: "Videos", href: "#"},
+            { label: "News", href: "#"},
         ]
     },
     {
         id: 2,
         title: "shop",
         links: [
-            {
-                label: "Characteers",
-                href: "#",
-            }
+            { label: "Shop DC", href: "#"},
+            { label: "Shop DC Collectibles", href: "#"}
         ]
     },
     {
         id: 3,
         title: "dc",
         links: [
-            {
-                label: "Characteers",
-                href: "#",
-            }
+            { label: "Terms Of Use", href: "#"},
+            { label: "Privacy policy(New)", href: "#"},
+            { label: "Ad Choices", href: "#"},
+            { label: "Advertising", href: "#"},
+            { label: "Jobs", href: "#"},
+            { label: "Subscriptions", href: "#"},
+            { label: "Talent Workshops", href: "#"},
+            { label: "CPSC Certificates", href: "#"},
+            { label: "Ratings", href: "#"},
+            { label: "Shop Help", href: "#"},
+            { label: "Contact Us", href: "#"},
         ]
     },
     {
         id: 4,
         title: "sites",
         links: [
-            {
-                label: "Characteers",
-                href: "#",
-            }
+            { label: "DC", href: "#"},
+            { label: "MAD Magazine", href: "#"},
+            { label: "DC Kids", href: "#"},
+            { label: "DC Universe", href: "#"},
+            { label: "DC Power Visa", href: "#"},
         ]
     },
 ]
@@ -49,59 +58,27 @@ export default function Footer() {
             <div className="container py-4 position-relative">
                 <img className="position-absolute end-0 bottom-img-footer z-0" src="src/assets/img/dc-logo-bg.png" alt="" />
                 <div className="d-flex text-light">
-                    <div className="me-5">
-                        <h2 className="text-uppercase fw-bold">dc comics</h2>
-                        <ul className="p-0 footer-text">
-                            <li><a href="">Characters</a></li>
-                            <li><a href="">Comics</a></li>
-                            <li><a href="">Movies</a></li>
-                            <li><a href="">TV</a></li>
-                            <li><a href="">Games</a></li>
-                            <li><a href="">Videos</a></li>
-                            <li><a href="">News</a></li>
-                        </ul>
 
-                        <h2 className="text-uppercase fw-bold mt-2">shop</h2>
-                        <ul className="p-0 footer-text">
-                            <li><a href="">Shop DC</a></li>
-                            <li><a href="">Shop DC Collectibles</a></li>
-                        </ul>
-                    </div>
-
-                    <div className="me-2">
-                        <h2 className="text-uppercase fw-bold">dc</h2>
-                        <ul className="p-0 footer-text">
-                            <li><a href="">Term Of Use</a></li>
-                            <li><a href="">Privacy Policy (new)</a></li>
-                            <li><a href="">Ad Choices</a></li>
-                            <li><a href="">Advertising</a></li>
-                            <li><a href="">Jobs</a></li>
-                            <li><a href="">Subcriptions</a></li>
-                            <li><a href="">Talent Workshops</a></li>
-                            <li><a href="">CPSC Certificates</a></li>
-                            <li><a href="">Ratings</a></li>
-                            <li><a href="">Shop Help</a></li>
-                            <li><a href="">Contact Us</a></li>
-                        </ul>
-                    </div>
-
-                    <div>
-                        <h2 className="text-uppercase fw-bold">sites</h2>
-                        <ul className="p-0 footer-text">
-                            <li><a href="">DC</a></li>
-                            <li><a href="">MAD Magazine</a></li>
-                            <li><a href="">DC Kids</a></li>
-                            <li><a href="">DC Universe</a></li>
-                            <li><a href="">DC Power Visa</a></li>
-                        </ul>
-                    </div>
+                    {footerSection.map((section) => (
+                        <div className="me-5" key={section.id}>
+                            <h2 className="text-uppercase fw-bold">{section.title}</h2>
+                            <ul className="m-0 p-0">
+                                {section.links.map((link, linkIndex) => (
+                                    <li key={linkIndex}>
+                                        <a className="fw-light footer-text" href={link.href}>{link.label}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                
                 </div>
             </div>
 
             <div className="bg-dark py-5 position-relative z-3">
                 <div className="container">
                     <div className="d-flex justify-content-between">
-                        <button type="button" class="btn btn-outline-primary text-light py-2 text-uppercase">sign up - now!</button> 
+                        <button type="button" class="btn btn-outline-primary text-light py-2 text-uppercase">sign up - now!</button>
 
                         <div className="d-flex align-items-center">
                             <h3 className="text-uppercase text-primary fw-bold m-0 px-3">follow us</h3>
